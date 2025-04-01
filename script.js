3e0c1c0614ca3e382d0287b0570e8bfd93874da0
anil reddy <lokaanilreddy5@gmail.com>
2025-04-01 11:50:58 +0530

    change

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-const encrypt = (payload, secret) => {
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
+
+const encrypt = (payload, secret, expiresIn = '1d') => {
   // your code here and return token
+  return jwt.sign(payload, secret, { expiresIn });
+
 };
 
 module.exports = encrypt;
